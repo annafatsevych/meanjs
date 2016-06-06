7e1c331cdd911b1e8f4206e232bf95fd5495840e
annafatsevych <anna.fatsevych@gmail.com>
2016-06-05 21:25:53 -0400

    Changed permissions

--- modules/freebies/server/policies/freebies.server.policy.js
+++ modules/freebies/server/policies/freebies.server.policy.js
@@ -41,6 +41,9 @@ exports.invokeRolesPolicies = function () {
     }, {
       resources: '/api/freebies/:freebieId',
       permissions: ['get']
+    }, {
+      resources: '/api/categories',
+      permissions: '*'
     }]
   }]);
 };
